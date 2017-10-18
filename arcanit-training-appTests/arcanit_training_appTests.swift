ab433e8ae2a65338fb9de035c9424d601490c011
Apple <artembalasov@MacBook-Pro-Artem.local>
2017-10-18 12:54:04 +0300

    Initial Commit

---
+++ arcanit-training-appTests/arcanit_training_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  arcanit_training_appTests.swift
+//  arcanit-training-appTests
+//
+//  Created by Артём Балашов on 18.10.2017.
+//  Copyright © 2017 Артём Балашов. All rights reserved.
+//
+
+import XCTest
+@testable import arcanit_training_app
+
+class arcanit_training_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
